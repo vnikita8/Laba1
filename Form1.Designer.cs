5bfec58f7258c8bf228591ad4c003bb467ec20e0
vnikita8 <vnikita123456@gmail.com>
2021-09-11 12:13:52 +0700

    1

---
+++ Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 25);
+            this.dataGridView1.MinimumSize = new System.Drawing.Size(5, 5);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(203, 198);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(203, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Проверить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 258);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Рандом";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(119, 258);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Все \"1\"";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 14);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Задание 1 (2 вариант)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 292);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 14);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Задание 2 (2 вариант)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(68, 336);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(131, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 361);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(203, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Перевести";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2",
+            "10"});
+            this.comboBox1.Location = new System.Drawing.Point(68, 309);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(40, 21);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "2",
+            "10"});
+            this.comboBox2.Location = new System.Drawing.Point(135, 309);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(40, 21);
+            this.comboBox2.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 312);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Из";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(116, 312);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "в";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 339);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Число:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(68, 390);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(131, 20);
+            this.textBox2.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(0, 393);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Результат:";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 416);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(203, 23);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "Закрыть";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 440);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Lab1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        public void RandomOrk()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 5; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = rnd.Next(1, 10);
+                }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int w, c, wc;
+
+            dataGridView1.AllowUserToAddRows = false;
+            for (int i = 0; i<5; i++)
+            {
+                dataGridView1.Columns.Add(i.ToString(), i.ToString());
+                dataGridView1.Rows.Add();
+                dataGridView1.Columns[i].Width = 40;
+                dataGridView1.Rows[i].Height = 40;
+            }
+            c = dataGridView1.Columns.Count;
+            w = dataGridView1.Columns[0].Width;
+            wc = c * w +3;
+            dataGridView1.Width = wc;
+            c = dataGridView1.Rows.Count;
+            w = dataGridView1.Rows[0].Height;
+            wc = c * w +3;
+            dataGridView1.Height = wc;
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int check = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if ((dataGridView1.Rows[i].Cells[j].Value != null) & (dataGridView1.Rows[j].Cells[i].Value != null))
+                        {
+                        try
+                        {
+                            int n = int.Parse(dataGridView1.Rows[i].Cells[j].Value.ToString());
+                            int m = int.Parse(dataGridView1.Rows[j].Cells[i].Value.ToString());
+                            if (dataGridView1.Rows[i].Cells[j].Value.ToString() != dataGridView1.Rows[j].Cells[i].Value.ToString())
+                            {
+                                check = 1;
+                                break;
+                            }
+                        }
+                        catch
+                        {
+                            check = 2;
+                            break;
+                        }
+                        }
+                    else
+                    {
+                        check = 4;
+                        break;
+                    }
+                }
+                if (check != 0)
+                {
+                    break;
+                }
+            }
+            if (check == 0)
+            {
+                MessageBox.Show("Симметрична", "Успешно");
+            }
+            else if (check == 2)
+            {
+                MessageBox.Show("Введите целочисленные значения", "Ошибка");
+            }
+            else if (check == 1)
+            {
+                MessageBox.Show("Не симметрична", "Успешно");
+            }
+            else
+            {
+                MessageBox.Show("Заполните матрицу", "Ошибка");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RandomOrk();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 5; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = 1;
+                }
+        }
+        
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Exception2 two = new Exception2();
+            two.Swit = comboBox1.Text + comboBox2.Text;
+            two.TextBox_1 = textBox1.Text;
+            textBox2.Text = two.GoDo();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+
+
+    public class PreException2
+    {
+        public string Swit { get; set; }
+        public string TextBox_1 { get; set; }
+
+    }
+    public class Exception2 : PreException2
+    {
+        public string GoDo()
+        {
+            switch (Swit)
+            {
+                case "22":
+                    bool bul1 = false;
+                    for (int i = TextBox_1.Length - 1; i >= 0; i--)
+                        if ((int.Parse(TextBox_1[i].ToString()) < 0) | (int.Parse(TextBox_1[i].ToString()) > 1))
+                        {
+                            bul1 = true;
+                        }
+                    if (bul1 == false)
+                    {
+                        return TextBox_1;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Некорректное число", "Ошибка");
+                        return null;
+                    }
+                case "1010":
+                    return TextBox_1;
+                case "210":
+                    //из 2 в 10
+                    if (TextBox_1 == null)
+                    {
+                        MessageBox.Show("Введите целочисленное значение", "Ошибка");
+                        return null;
+                    }
+                    else
+                    {
+                        try
+                        {
+                            string res = Convert.ToInt32(TextBox_1, 2).ToString();
+                            return res;
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Введите целочисленное значение", "Ошибка");
+                            return null;
+                        }
+                    }
+                case "102":
+                    string bin = Convert.ToString(int.Parse(TextBox_1), 2);
+                    return bin;
+                default:
+                    {
+                        MessageBox.Show("Вы не выбрали систему счисления", "Ошибка");
+                        return null;
+                    }
+            }
+        }
+    }
+
+
+}
